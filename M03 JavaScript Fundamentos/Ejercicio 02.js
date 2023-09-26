0e86fdaf36948400e26eab5914dcce9629059ce0
LucaNor <lucascythe@gmail.com>
2023-09-26 14:12:20 -0300

    Homework hasta el M4

--- M03 JavaScript Fundamentos/Ejercicio 02.js
+++ M03 JavaScript Fundamentos/Ejercicio 02.js
@@ -4,34 +4,52 @@
 function devolverString(string) {
    // Debe retornar un string.
    // Tu código:
+   return string;   
 }
+
+devolverString("Lucas");
 
 // ⛔️ "X" e "Y" son números.
 
 function suma(x, y) {
    // Retorna el resultado de su suma.
    // Tu código:
+   return x + y;
 }
+
+suma(15, 9);
 
 function resta(x, y) {
    // Retorna el resultado de la resta.
    // Tu código:
+   return x - y;
 }
+
+resta(20,14);
 
 function divide(x, y) {
    // Retorna el resultado de su división.
    // Tu código:
+   return x / y;
 }
+
+divide(9,3);
 
 function multiplica(x, y) {
    // Retorna el resultado de su multiplicación.
    // Tu código:
+   return x * y;
 }
+
+multiplica(9,60);
 
 function obtenerResto(x, y) {
    // Obten el resto de la división de "x" entre "y".
    // Tu código:
+   return x % y;
 }
+
+obtenerResto(56,9);
 
 /*⚠️ NO MODIFIQUES NADA DEBAJO DE ESTO ⚠️*/
 module.exports = {
